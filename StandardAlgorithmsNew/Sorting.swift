6c4c16d00cb508a76f7933322955dda1a110814b
Ssnehnil Ghosh <ghosh.s@etoncollege.org.uk>
2021-11-25 12:27:05 +0000

    Finished Bubble Sort testing, now doing Quick Sort testing.

--- StandardAlgorithmsNew/Sorting.swift
+++ StandardAlgorithmsNew/Sorting.swift
@@ -9,10 +9,47 @@ import Foundation
 
 class Sorting {
     func bubbleSort(_ data: [Int]) -> [Int] {
+        var data = data
         if data.count == 0 {
             return []
-        } else {
-            return [1,2,3,5,9]
         }
+        let lastMarker = data.count - 1
+        var listSorted = true
+        while listSorted == true {
+          listSorted = false
+            for i in 0..<lastMarker {
+            if data[i] > data[i+1] {
+              let temp = data[i+1]
+              data[i+1] = data[i]
+              data[i] = temp
+              listSorted = true
+            }
+          }
+        }
+        return data
     }
+    
+    func quickSort(_ data: [Int]) -> [Int] {
+        let data = data
+        let pivot = data[0] //Pivot
+        var leftArray :[Int] = [] //Left array where smaller numbers will be
+        var rightArray:[Int] = [] // Right array for the bigger numbers
+        var returnArray:[Int] = [] // Final array which the left & right array will add to
+        for i in 1..<data.count { // Loop to sort numbers into left or right array
+          if data[i] < pivot {
+            leftArray.append(data[i])
+          } else {
+            rightArray.append(data[i])
+          }
+        }
+        if leftArray.count < 1 { //If statement to check if list is sorted or is unsorted
+          return data
+        } else {
+          returnArray.append(contentsOf: quickSort(leftArray))
+          returnArray.append(pivot)
+          returnArray.append(contentsOf: quickSort(rightArray))
+        }
+        
+        return returnArray
+      }
 }
--- StandardAlgorithmsTests/SortingTest.swift
+++ StandardAlgorithmsTests/SortingTest.swift
@@ -22,4 +22,18 @@ class SortingTest: XCTestCase {
             XCTAssertEqual(actual, testCase.expected)
         }
     }
+    
+    func testQuickSortWithUnsortedIntegerArrayReturnsSortedIntegerArray() {
+        //arrange
+        let sorting = Sorting()
+        
+        let testCases = [(input: [3,5,1,2,9], expected: [1,2,3,5,9]), (input: [3,7,1,2,9], expected: [1,2,3,7,9]), (input: [], expected: [])]
+        
+        //act
+        //assert
+        for testCase in testCases {
+            let actual = sorting.quickSort(testCase.input)
+            XCTAssertEqual(actual, testCase.expected)
+        }
+    }
 }
